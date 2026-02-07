3f1608f003728be8dc0266765753bb03617ac6f6
Jon Young <youngt.jonathan>
2026-02-07 16:10:21 -0600

    no more military time on form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -441,13 +441,14 @@ const ContactForm = () => {
                 Event Start Time
               </label>
               <input
-                type="time"
+                type="text"
                 id="event-time"
                 name="event-time"
                 value={formData.eventTime}
                 onChange={handleChange}
                 onFocus={() => handleFocus("event-time")}
                 onBlur={handleBlur}
+                placeholder="7:00 PM"
               />
             </div>
 
